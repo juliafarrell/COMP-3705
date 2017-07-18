86911f3053723be82e92bc7a125a1eafad4b4aab
Julia Farrell <juliafarrell1.618@gmail.com>
2017-07-18 17:33:55 -0400

    Completed COMP-3705 lab 8

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
     uri: process.env.MONGODB_URI
       || process.env.MONGOHQ_URL
       || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
-      || 'mongodb://localhost/comp3705'
+      || 'mongodb://heroku_hvqjw6cw:hv2gktflvjdleqti2m98hrokis@ds163232.mlab.com:63232/heroku_hvqjw6cw'
   }
 };
