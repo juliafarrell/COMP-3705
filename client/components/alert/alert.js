b43621d55fdffc81113b26b6425b19e960836edc
Julia Farrell <juliafarrell1.618@gmail.com>
2017-08-09 22:35:44 -0600

    lots of errors

---
+++ client/components/alert/alert.js
@@ -0,0 +1,16 @@
+import angular from 'angular';
+
+export default angular.module('comp3705App.createUserModal').controller('AlertController', function($scope) {
+  $scope.alerts = [
+      { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+      { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+  ];
+
+  $scope.addAlert = function() {
+    $scope.alerts.push({msg: 'Another alert!'});
+  };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+});
